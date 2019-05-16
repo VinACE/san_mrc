8f3b2782a2f914ad8758bfce03915c6c9d794048
vinace <vinsent.storage@gmail.com>
2019-05-16 19:46:06 +0530

    increasing the dock length size

--- prepro.py
+++ prepro.py
@@ -32,7 +32,7 @@ NLP = spacy.load('en', disable=['vectors', 'textcat', 'parser'])
 
 def load_data(path, is_train=True, v2_on=False):
     rows = []
-    with open(path, encoding="utf8") 
+    with open(path, encoding="utf8"):
         data = json.load(f)['data']
     for article in tqdm.tqdm(data, total=len(data)):
         for paragraph in article['paragraphs']:
